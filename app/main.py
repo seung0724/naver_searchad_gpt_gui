9b84aa118e8b1f71f26affc3b3f63851aae2f037
seung0724 <seung20100724@gmail.com>
2025-04-20 22:22:28 +0900

    fix template folder path

--- app/main.py
+++ app/main.py
@@ -1,5 +1,9 @@
 from flask import Flask, render_template, request, redirect, url_for, session
 from flask_session import Session
+import os
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+TEMPLATE_DIR = os.path.join(BASE_DIR, '..', 'templates')
 
 app = Flask(__name__)
 app.secret_key = 'your_secret_key'
